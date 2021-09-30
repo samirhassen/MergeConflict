ed443b0705d749f72faeeba27eafca1ed2f00dda
EmilFrick <emil.a.frick@gmail.com>
2021-09-30 11:57:54 +0200

    Frm nother b

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace MergeConflict
         static void Main(string[] args)
         {
             Console.WriteLine("Helloq Worqdld!");
+            string myBad = "Im sorry that im not sorry";
+            for (int i = 0; i < 9999; i++)
+            {
+                Console.WriteLine(myBad);
+            }
         }
     }
 }
