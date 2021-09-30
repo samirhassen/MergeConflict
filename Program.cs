b1b03700c53dc34b40775200f2c744c80e199400
Hassen <samir.hassen@sogeti.com>
2021-09-30 11:26:34 +0200

    init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MergeConflict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
